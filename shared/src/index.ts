49df793b1e80d40f8af9df511e42633e164015e9
r1laun <r1laun@users.noreply.github.com>
2026-09-23 16:12:45 +0500

    Revert "net: migrate from socket.io to raw WebSocket + Cloudflare worker"
    This reverts commit 6dec984386711a466300b5a6a39909a0529c7839.

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,6 +1,5 @@
 export * from './types.js';
 export * from './events.js';
-export * from './protocol.js';
 export * from './difficulty.js';
 export * from './validator/index.js';
 export * from './generator.js';
